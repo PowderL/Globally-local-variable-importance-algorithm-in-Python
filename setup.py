608c80c242b96d13d3a48f5056fe511fc5480ec9
Tao Li <lp1559345469@gmail.com>
2020-09-17 14:41:43 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ with open('README.rst', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 install_requires = [
     "scikit-learn >= 0.20.0",
+    "pandas >= 0.24.0"
     "numpy >= 1.11.0",
     "scipy >= 0.17.0",
     "joblib >= 0.11.0"]
 setuptools.setup(name='glvi',
-                 version='0.1.6',
+                 version='0.1.7',
                  description='Local variable importance from a global model',
                  long_description=long_description,
                  long_description_content_type="text/markdown",
@@ -23,11 +24,12 @@ setuptools.setup(name='glvi',
                  install_requires = install_requires,
                  classifiers=[
                      "Programming Language :: Python :: 3",
-                     'Programming Language :: Python :: 3.6',
-                     'Programming Language :: Python :: 3.7',
-                     'Programming Language :: Python :: 3.8',
+                     "Programming Language :: Python :: 3.5",
+                     "Programming Language :: Python :: 3.6",
+                     "Programming Language :: Python :: 3.7",
+                     "Programming Language :: Python :: 3.8",
                      "Intended Audience :: Science/Research",
-                     "Operating System :: Microsoft :: Windows 10",
+                     "Operating System :: Microsoft :: Windows :: Windows 10",
                      "Operating System :: POSIX",
                      "Operating System :: Unix",
                      "Operating System :: MacOS",
